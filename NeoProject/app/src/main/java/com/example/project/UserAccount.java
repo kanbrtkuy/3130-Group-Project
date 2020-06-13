d58cc961d9455d9fa148f8000c4411e484dc332c
Hanwen Zhang <hn309286@dal.ca>
2020-06-13 12:54:34 -0300

    Update UserAccount.java

--- NeoProject/app/src/main/java/com/example/project/UserAccount.java
+++ NeoProject/app/src/main/java/com/example/project/UserAccount.java
@@ -5,6 +5,42 @@ public class UserAccount {
     private String EmailAddress;
     private String Password;
 
+    public UserAccount(String Username, String EmailAddress, String Password){
+        this.Username = Username;
+        this.EmailAddress = EmailAddress;
+        this.Password = Password;
+    }
 
+    public String getUsername(){return Username;}
+
+    public String getEmailAddress(){return EmailAddress;}
+
+    public String getPassword(){return Password;}
+
+    public void setUsername(String Username ) {this.Username = Username;}
+
+    public void setEmailAddress(String EmailAddress){this.EmailAddress = EmailAddress;}
+
+    public void setPassword(String Password){ this.Password = Password;}
+
+    public boolean PasswordLength(){
+        int l = Password.length();
+        if (l >= 6){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public boolean ValidEmail(){
+        int l = EmailAddress.length();
+        for (int i = 0; i < l ; i ++){
+            if (EmailAddress.charAt(i) == '@' ){
+                return true;
+            }
+        }
+        return false;
+    }
 
 }
