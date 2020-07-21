35b6452072a34be7b325d986ac099c911f663ea7
Jiaxin Zhu <jx305757@dal.ca>
2020-07-21 00:42:08 -0300

    for iteration 3, add a new UI test for record the training time, it is called ClockTest.

---
+++ NeoProjectVer2/app/src/androidTest/java/com/example/neoprojectver2/ClockTest.java
@@ -0,0 +1,38 @@
+package com.example.neoprojectver2;
+
+import androidx.test.filters.LargeTest;
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+
+@LargeTest
+@RunWith(AndroidJUnit4ClassRunner.class)
+public class ClockTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void ClockTest() {
+
+        onView(withContentDescription(R.string.open))
+                .perform(click());
+        onView(withText("Start countdown timer"))
+                .perform(click());
+
+    }
+    
+}
