a0b28710c4c289795a6dfab55d6c4e9c85410298
Jiaxin Zhu <jx305757@dal.ca>
2020-07-21 00:40:04 -0300

    for iteration 3, add a new test for emergency call, it is called EmergencyTest, it is for the emergency call function.

---
+++ NeoProjectVer2/app/src/androidTest/java/com/example/neoprojectver2/EmergencyTest.java
@@ -0,0 +1,41 @@
+package com.example.neoprojectver2;
+
+import androidx.test.filters.LargeTest;
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+
+@LargeTest
+@RunWith(AndroidJUnit4ClassRunner.class)
+public class EmergencyTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void EmergencyTest() {
+
+        onView(withContentDescription(R.string.open))
+                .perform(click());
+        onView(withText("Emergency Call"))
+                .perform(click());
+
+    }
+
+
+
+
+}
